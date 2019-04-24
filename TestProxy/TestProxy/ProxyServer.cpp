e1ebdf0ce6c2561f7028c5586fd14e17ec5df5c5
Nhinlee <48386426+Nhinlee@users.noreply.github.com>
2019-04-24 19:16:24 +0700

    time_1

---
+++ TestProxy/TestProxy/ProxyServer.cpp
@@ -0,0 +1,166 @@
+﻿// Server.cpp : Defines the entry point for the console application.
+//
+
+#include "pch.h"
+#include "ProxyServer.h"
+#include "stdafx.h"
+#include "afxsock.h"
+#include "Proxy_Parse.h"
+#include <thread>
+//#include<string>
+//#include<WinSock2.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+bool IsEndHeader(string a)
+{
+	int n = a.size();
+	if (n < 4)
+		return false;
+	if (a[n - 4] == '\r' && a[n - 3] == '\n' && a[n - 2] == '\r' && a[n - 1] == '\n')
+		return true;
+	else return false;
+}
+// The one and only application object
+
+CWinApp theApp;
+
+using namespace std;
+
+int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
+{
+	int nRetCode = 0;
+
+	// initialize MFC and print and error on failure
+	if (!AfxWinInit(::GetModuleHandle(NULL), NULL, ::GetCommandLine(), 0))
+	{
+		// TODO: change error code to suit your needs
+		_tprintf(_T("Fatal Error: MFC initialization failed\n"));
+		nRetCode = 1;
+	}
+	else
+	{
+		// TODO: code your application's behavior here.
+
+		// Khoi tao thu vien Socket
+		if (AfxSocketInit() == FALSE)
+		{
+			cout << "Khong the khoi tao Socket Library";
+			return FALSE;
+		}
+
+		CSocket ProxySocket; //cha
+		// Tao socket cho server, dang ky port la 8888, giao thuc TCP
+		if (ProxySocket.Create(8888, SOCK_STREAM, NULL) == 0) //SOCK_STREAM or SOCK_DGRAM.
+		{
+			cout << "Khoi tao that bai !!!" << endl;
+			cout << ProxySocket.GetLastError();
+			return FALSE;
+		}
+		else
+		{
+				cout << "Server khoi tao thanh cong !!!" << endl;
+		}
+
+		// Dung socket nay de listen cac request tu client:
+
+		if (ProxySocket.Listen(5) == FALSE)
+		{
+			cout << "Khong the lang nghe tren port nay !!!" << endl;
+			ProxySocket.Close();
+			return FALSE;
+		}
+		char request[5001] = { 0 }, dname[1001], ip[16], body_res[5001] = { 0 }, header_res[1001] = { 0 };
+		while(1)
+		{
+			CSocket Connector;
+			if (ProxySocket.Accept(Connector))
+			{
+
+				cout << "Da co Client ket Noi !!!" << endl << endl;
+
+				memset(request, 0, sizeof request);
+				memset(dname, 0, sizeof dname);
+				memset(ip, 0, sizeof ip);
+
+				int revd = 0;
+				if ((revd = Connector.Receive(request, 5000, 0)) > 0)
+				{
+					cout << "nhan request thanh cong " << endl;
+					cout << "tong do dai byte: " << revd << endl;
+					cout << request << "\n\n";
+				}
+				//
+				if (IsHTTPs(request))
+				{
+					cout << "is HTTPS" << endl;
+					continue;
+					
+				}
+				//
+				if (GetDomainName(request, dname) == false)
+				{
+					cout << "Get Domain Name False !!!";
+				}
+				//else cout << dname << endl;
+				// GET IP:
+
+				hostent *remoteHost = gethostbyname(dname);
+				int i = 0;
+				in_addr addr;
+				while (remoteHost->h_addr_list[i] != 0) {
+					addr.s_addr = *(u_long *)remoteHost->h_addr_list[i++];
+					strcpy(ip, inet_ntoa(addr));
+					printf("\tIPv4 Address #%d: %s\n", i, inet_ntoa(addr));
+				}
+				//cout << ip << endl;
+				// Tao Socket proxy moi de ket noi toi trang web:
+				CSocket Proxy_Server;
+				Proxy_Server.Create();
+				// Ket noi toi server:
+				if (Proxy_Server.Connect(CA2W(ip), 80) != 0)
+				{
+					cout << "Ket noi toi Web thanh cong !!!" << endl;
+				}
+				revd = Proxy_Server.Send(request, revd, 0);
+
+
+				// Nhan va gui ve lai cho client:
+				string head;
+				int id = 0;
+				while (!IsEndHeader(head))
+				{
+					Proxy_Server.Receive(header_res + id, 1, 0);
+					head.push_back(header_res[id]);
+					cout << header_res[id];
+					id++;
+				}
+				Connector.Send(header_res, id, 0);
+				cout << "Xong header roi nhe !" << endl << endl;
+
+
+
+				id = 0;
+				while ((revd = Proxy_Server.Receive(body_res, 5000, 0)) > 0)
+				{
+					Connector.Send(body_res, revd, 0);
+					cout << "Tong byte nhan lan thu " << id << " : " << revd << endl;
+					//if (id == 0)
+					//cout << body_res << endl;
+					id++;
+					memset(body_res, 0, sizeof body_res);
+				}
+				// Đóng kết nối tới server web:
+				Proxy_Server.Close();
+			}
+			Connector.Close();
+		}
+		ProxySocket.Close();
+	}
+
+	return nRetCode;
+}
+
---
+++ TestProxy/TestProxy/Proxy_Parse.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include "Proxy_Parse.h"
+
+bool GetDomainName(char *request, char *dname)
+{
+	int n = strlen(request), pos = 0, i = 0;
+	n -= 5;
+	for (; i < n; i++)
+	{
+		if (request[i] == 'H' && request[i+1] == 'o' && request[i+2] == 's' && request[i+3] == 't'
+			&& request[i+4] == ':')
+		{
+			pos = i + 6;
+			break;
+		}
+	}
+	if (i == n)
+		return false;
+	i = 0;
+	while (request[pos] != '\r' && request[pos] != ':')// Nếu là cổng 443 (HTTPs) thì phải chạy tới : và dừng lại.
+	{
+		dname[i] = request[pos];
+		pos++;
+		i++;
+	}
+	dname[i] = '\0';
+	return true;
+
+}
+bool IsHTTPs(char *request)
+{
+	int i = 0, n = strlen(request);
+	while (i < n)
+	{
+		if (request[i] == ':' && request[i + 1] == '4' && request[i + 2] == '4'
+			&& request[i + 3] == '3')
+			return true;
+		i++;
+	}
+	return false;
+}
